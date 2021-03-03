1266a90a22b167f13638b3d6ebbc9565d29c08b2
Takahiro <hogehoge@gachapin.jp>
2021-03-03 00:41:07 -0800

    Exporting a module named MMDParser for Three.js relying on it

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-export { CharsetEncoder } from './src/CharsetEncoder'
-export { Parser } from './src/Parser'
+import { CharsetEncoder } from './src/CharsetEncoder'
+import { Parser } from './src/Parser'
+
+var MMDParser = {
+  CharsetEncoder: CharsetEncoder,
+  Parser: Parser
+};
+
+export { MMDParser, CharsetEncoder, Parser };
+
